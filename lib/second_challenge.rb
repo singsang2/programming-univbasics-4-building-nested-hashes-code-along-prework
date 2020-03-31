0946b2c069237e42334d50fccfd147334c97e00e
Sung Bae <singsang2@gmail.com>
2020-03-31 20:53:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,16 @@
-
-epic_tragedy = {
-  :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-  },
-  :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+def second_challenge
+  epic_tragedy = {
+    :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+    },
+    :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+    }
   }
-}
+end
